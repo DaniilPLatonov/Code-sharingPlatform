5c1cb063ce3352f12c4a03efb8a18bfc95be3343
DaniilPlatonov <d_platonov@internet.ru>
2023-06-08 21:42:10 +0300

    Initial commit

--- Code-sharing Platform/task/src/main/java/platform/persistence/CodeResponse.java
+++ Code-sharing Platform/task/src/main/java/platform/persistence/CodeResponse.java
@@ -1,41 +1,19 @@
 package platform.persistence;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jdk.jfr.DataAmount;
-
-import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-
-@Entity
-@Table(name = "snippets")
-@JsonIgnoreProperties(value = {"id"})
 public class CodeResponse {
-    @Column(name = "code", columnDefinition = "VARCHAR(4096)")
+    @JsonProperty("code")
     private String currentCode;
-    @Column(name = "date")
+    @JsonProperty("date")
     private LocalDateTime loadDate;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", updatable = false, nullable = false)
-    private Long id;
-
-    public CodeResponse() {
-
-    }
 
     public String getCurrentCode() {
         return currentCode;
     }
 
-    public void setCurrentCode(String currentCode) {
-        this.currentCode = currentCode;
-    }
-
-    public void setLoadDate(LocalDateTime loadDate) {
-        this.loadDate = loadDate;
-    }
 
     public String getLoadDate() {
         return loadDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSS"));
@@ -44,13 +22,5 @@ public class CodeResponse {
     public CodeResponse(String currentCode, LocalDateTime loadDate) {
         this.currentCode = currentCode;
         this.loadDate = loadDate;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
     }
 }
